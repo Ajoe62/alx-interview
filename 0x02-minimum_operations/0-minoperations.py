4e9c39a082421dde0c00afa6b4232cedf94637e9
Ajoe62 <ijeborjoe@gmail.com>
2024-05-09 03:42:57 -0700

    file update

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" write a method that calculates the fewest number of operations needed to result in exactly n H characters in the file """
+""" method that calculates the fewest number of operations """
 
 
 def minOperations(n):
