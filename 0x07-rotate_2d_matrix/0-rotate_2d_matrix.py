2adcae44582eb735b38a7d6147b7c35b14e042f8
Ajoe62 <ijeborjoe@gmail.com>
2024-06-13 04:37:51 -0700

    0-rotate_2d_matrix.py

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+This script rotates a given n x n 2D matrix 90 degrees clockwise in-place.
+"""
+
+def rotate_2d_matrix(matrix):
+    """
+    Rotate a given n x n 2D matrix 90 degrees clockwise in-place.
+
+    Parameters:
+    matrix (list[list[int]]): A 2D list representing the matrix to be rotated.
+
+    Returns:
+    None
+    """
+    # Transpose the matrix
+    for i in range(len(matrix)):
+        for j in range(i, len(matrix)):
+            matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+    
+    # Reverse each row
+    for i in range(len(matrix)):
+        matrix[i] = matrix[i][::-1]
---
+++ 0x07-rotate_2d_matrix/README.md
@@ -0,0 +1,13 @@
+# Matrix Rotation Script
+
+This repository contains a Python script designed to rotate a given n x n 2D matrix 90 degrees clockwise in-place. The script adheres to specific coding standards and requirements, including:
+
+- The script begins with the shebang line `#!/usr/bin/python3`.
+- It follows the PEP 8 style guide as enforced by pycodestyle (version 2.8.0).
+- No external modules are imported.
+- All functions are documented with docstrings.
+- The script is executable.
+
+## Usage
+
+To execute the script, navigate to the project directory in your terminal and run:
