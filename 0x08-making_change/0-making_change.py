16eab2b4058dfc4c86be331bb913eb1a0698fa32
Ajoe62 <ijeborjoe@gmail.com>
2024-06-20 12:35:26 -0700

    add task file

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
+"""
+Calculate fewest number of coins needed to
+meet a given amount total
+"""
+
 
 def makeChange(coins, total):
-    """
-    Calculate the fewest no of coins to make up a certain amount of money.
-    """
-    dp = [total + 1] * (total + 1)
-    dp[0] = 0
-
-    for t in range(1, total + 1):
-        for c in coins:
-            if t - c >= 0:
-                dp[t] = min(dp[t], 1 + dp[t - c])
-
-    return dp[total] if dp[total] != total + 1 else -1
+    """ fewest number of coins needed to meet total """
+    if total <= 0:
+        return 0
+    # sort the coins in descending order
+    coins.sort(reverse=True)
+    change = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        temp = total // coin
+        change += temp
+        total -= (temp * coin)
+    if total != 0:
+        return -1
+    return change
