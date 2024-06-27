c1899b12711438dcb7af7b0ca33b2efb46c0a8ff
Ajoe62 <ijeborjoe@gmail.com>
2024-06-27 10:54:37 -0700

    island_perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""Solutionto the Island Perimeter Problem"""
+
+def island_perimeter(grid):
+    """
+    calculate the perimeter of the island
+    """
+
+    perim = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if (grid[i][j] == 1):
+                if (i <= 0 or grid[i - 1][j] == 0):
+                    perim += 1
+                if (i >= len(grid) -1 or grid[i + 1][j] == 0):
+                    perim += 1
+                if (j <= 0 or grid[i][j - 1] == 0):
+                    perim += 1
+                if (j >= len(grid[i]) - 1 or grid[i][j + 1] == 0):
+                    perim += 1
+    return perim
